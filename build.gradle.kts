5a270e30bc07ed545dd6b361dfc4a9c60ed298e3
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-11-17 09:50:47 +0100

    Add static jarfile name to prevent errors while building docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ group = "io.kni.thingoo"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+tasks.bootJar {
+	archiveFileName.set("thingoo-backend.jar")
+}
+
 repositories {
 	mavenCentral()
 }
