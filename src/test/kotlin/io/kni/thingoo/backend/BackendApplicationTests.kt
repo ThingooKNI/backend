f79b4eb51bc5007b364e1d14909bdbe729dea806
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-06 22:40:33 +0100

    Run tests in development profile

--- src/test/kotlin/io/kni/thingoo/backend/BackendApplicationTests.kt
+++ src/test/kotlin/io/kni/thingoo/backend/BackendApplicationTests.kt
@@ -2,8 +2,10 @@ package io.kni.thingoo.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("development")
 class BackendApplicationTests {
 
 	@Test
