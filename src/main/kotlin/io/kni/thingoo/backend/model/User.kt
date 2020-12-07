62a03b853af4fba1c6de277cb658626a105987d6
BartlomiejRasztabiga <contact@rasztabiga.me>
2020-12-07 18:21:47 +0100

    Don't use wildcard imports

--- src/main/kotlin/io/kni/thingoo/backend/model/User.kt
+++ src/main/kotlin/io/kni/thingoo/backend/model/User.kt
@@ -1,7 +1,16 @@
 package io.kni.thingoo.backend.model
 
-import java.util.*
-import javax.persistence.*
+import java.util.UUID
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
+import javax.persistence.Table
 
 @Entity
 @Table(name = "users")
