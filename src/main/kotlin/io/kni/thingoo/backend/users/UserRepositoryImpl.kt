e86429155c0615fa0a43c3000b091dba84a40f1f
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-06 23:57:43 +0100

    Reformat code

--- src/main/kotlin/io/kni/thingoo/backend/users/UserRepositoryImpl.kt
+++ src/main/kotlin/io/kni/thingoo/backend/users/UserRepositoryImpl.kt
@@ -1,24 +1,18 @@
 package io.kni.thingoo.backend.users
 
-import io.kni.thingoo.backend.users.exceptions.NoViewUsersRoleException
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.client.HttpClientErrorException
-import java.net.URI
 import java.util.Optional
 import java.util.UUID
 
 @Component
-class UserRepositoryImpl(private val keycloakRestTemplate: KeycloakRestTemplate) : UserRepository {
+class UserRepositoryImpl : UserRepository {
 
     @Value("\${keycloak.auth-server-url}")
     private lateinit var keycloakServerUrl: String
 
     @Value("\${keycloak.realm}")
     private lateinit var keycloakRealm: String
-
 
     override fun findByUsername(username: String): Optional<User> {
         TODO("Not yet implemented")
