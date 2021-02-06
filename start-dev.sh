1267b8cd2c34b685ea80f928cd5f0bb2d78fba59
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-06 22:00:43 +0100

    Add script to rebuild backend image

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 docker-compose down
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
