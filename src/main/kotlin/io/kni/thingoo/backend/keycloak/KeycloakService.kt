c27c5a8729d8dc375d64300f921a496f85f366e4
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-07 01:00:55 +0100

    Add proper Keycloak config

---
+++ src/main/kotlin/io/kni/thingoo/backend/keycloak/KeycloakService.kt
@@ -0,0 +1,35 @@
+package io.kni.thingoo.backend.keycloak
+
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class KeycloakService {
+
+    @Value("\${keycloak.auth-server-url}")
+    private lateinit var keycloakServerUrl: String
+
+    @Value("\${keycloak.realm}")
+    private lateinit var keycloakRealm: String
+
+    @Value("\${keycloak.resource}")
+    private lateinit var keycloakClientId: String
+
+    @Value("\${app.keycloak.admin-user.username}")
+    private lateinit var keycloakAdminUsername: String
+
+    @Value("\${app.keycloak.admin-user.password}")
+    private lateinit var keycloakAdminPassword: String
+
+    fun getInstance(): Keycloak {
+        return KeycloakBuilder.builder()
+            .serverUrl(keycloakServerUrl)
+            .realm(keycloakRealm)
+            .username(keycloakAdminUsername)
+            .password(keycloakAdminPassword)
+            .clientId(keycloakClientId)
+            .build();
+    }
+}
--- src/main/kotlin/io/kni/thingoo/backend/users/UserRepositoryImpl.kt
+++ src/main/kotlin/io/kni/thingoo/backend/users/UserRepositoryImpl.kt
@@ -1,25 +1,27 @@
 package io.kni.thingoo.backend.users
 
+import io.kni.thingoo.backend.keycloak.KeycloakService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.Optional
 import java.util.UUID
 
 @Component
-class UserRepositoryImpl : UserRepository {
+class UserRepositoryImpl(private val keycloakService: KeycloakService) : UserRepository {
 
-    @Value("\${keycloak.auth-server-url}")
-    private lateinit var keycloakServerUrl: String
 
-    @Value("\${keycloak.realm}")
-    private lateinit var keycloakRealm: String
 
     override fun findByUsername(username: String): Optional<User> {
         TODO("Not yet implemented")
     }
 
     override fun findAll(): List<User> {
-        TODO("Not yet implemented")
+
+        val keycloakInstance = keycloakService.getInstance()
+
+        println(keycloakInstance)
+
+        return listOf()
     }
 
     override fun findById(id: UUID): Optional<User> {
