bb35fdffa221486929d4c7d6041fbcc93c522f9a
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-10 18:42:10 +0100

    Add User data class

--- src/main/kotlin/io/kni/thingoo/backend/users/User.kt
+++ src/main/kotlin/io/kni/thingoo/backend/users/User.kt
@@ -1,18 +1,3 @@
 package io.kni.thingoo.backend.users
 
-import io.kni.thingoo.backend.roles.Role
-import java.util.UUID
-
-class User(
-    val uuid: UUID = UUID.randomUUID(),
-
-    var username: String,
-
-    var password: String,
-
-    var isActive: Boolean,
-
-    var fullName: String?,
-
-    var roles: MutableSet<Role> = mutableSetOf(),
-)
+data class User(val username: String, val firstName: String, val lastName: String, val email: String)
