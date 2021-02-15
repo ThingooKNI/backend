2cd6c679819bc467535178fd2d4f7d745c9aa126
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-15 19:21:59 +0100

    Add CORS config

---
+++ src/main/kotlin/io/kni/thingoo/backend/config/CorsConfiguration.kt
@@ -0,0 +1,25 @@
+package io.kni.thingoo.backend.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration {
+
+    @Value("#{'\${app.cors-origins}'.split(',')}")
+    private lateinit var corsAllowedOrigins: List<String>
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedOrigins(*corsAllowedOrigins.toTypedArray())
+                    .allowedHeaders("*")
+            }
+        }
+    }
+}
