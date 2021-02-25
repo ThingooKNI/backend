25e98366138e8bcbf5855c270b471ddf29f07da0
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-02-25 22:29:38 +0100

    Enable web security debug mode

--- src/main/kotlin/io/kni/thingoo/backend/config/SecurityConfig.kt
+++ src/main/kotlin/io/kni/thingoo/backend/config/SecurityConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Value("\${app.keycloak.webapp-client-id}")
