47eb8bf0fcbab3cf15bd3051b202fd5ccb588081
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-20 21:38:00 +0100

    Refactor PatchUtils.patchEntity

--- src/main/kotlin/io/kni/thingoo/backend/utils/PatchUtils.kt
+++ src/main/kotlin/io/kni/thingoo/backend/utils/PatchUtils.kt
@@ -8,16 +8,21 @@ class PatchUtils {
     companion object {
         fun patchEntity(patch: Map<String, Any>, patchDtoClass: KClass<*>, entity: Any) {
             val patchDtoFields = patchDtoClass.declaredMemberProperties
-            patchDtoFields.forEach {
-                if (patch.keys.contains(it.name)) {
-                    val patchValue = patch[it.name]!!
+
+            patchDtoFields.forEach { field ->
+                val fieldName = field.name
+                val fieldType = field.returnType
+
+                if (patch.keys.contains(fieldName)) {
+                    val patchValue = patch[fieldName]!!
+
                     when {
-                        ReflectionUtils.isExactTypeOf(patchValue, it.returnType) -> {
-                            ReflectionUtils.setInstanceProperty(entity, it.name, patchValue)
+                        ReflectionUtils.isExactTypeOf(patchValue, fieldType) -> {
+                            ReflectionUtils.setInstanceProperty(entity, fieldName, patchValue)
                         }
-                        ReflectionUtils.isEnumTypeOf(patchValue, it.returnType) -> {
-                            val enumValue = ReflectionUtils.createEnumFromString(it.returnType, patchValue.toString())
-                            ReflectionUtils.setInstanceProperty(entity, it.name, enumValue)
+                        ReflectionUtils.isEnumTypeOf(patchValue, fieldType) -> {
+                            val enumValue = ReflectionUtils.createEnumFromString(fieldType, patchValue.toString())
+                            ReflectionUtils.setInstanceProperty(entity, fieldName, enumValue)
                         }
                         else -> {
                             throw Exception("Patch entry field has invalid type")
